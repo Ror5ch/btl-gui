59d5df41f639bc32d55f54b6b936ddb90b1f14a7
Sedrick William Weinschenk <sedrick.william.weinschenk@cern.ch>
2022-03-23 15:08:56 +0100

    Upload New File

---
+++ GUI_global.py
@@ -0,0 +1,147 @@
+import Tkinter as tk
+
+root = tk.Tk()
+### GBT_SCA4 onoff buttons ###
+FE9_ALDO_En2_onoff_button4 = tk.Button(), FE9_ALDO_En1_onoff_button4 = tk.Button()
+PCC_B_EN_IV8_1_onoff_button4 = tk.Button(), PCC_B_EN_IV8_2_onoff_button4 = tk.Button()
+cSS_B_onoff_button4 = tk.Button(), FE10_ALDO_En1_onoff_button4 = tk.Button()
+FE10_ALDO_En2_onoff_button4 = tk.Button(), cEN_B_onoff_button4 = tk.Button()
+FE7_ALDO_En1_onoff_button4 = tk.Button(), FE7_ALDO_En2_onoff_button4 = tk.Button()
+eSS_B_onoff_button4 = tk.Button(), FE3_ALDO_En1_onoff_button4 = tk.Button()
+FE5_ALDO_En1_onoff_button4 = tk.Button(), oEN_B_onoff_button4 = tk.Button()
+FE6_ALDO_En1_onoff_button4 = tk.Button(), FE6_ALDO_En2_onoff_button4 = tk.Button()
+FE12_ALDO_En2_onoff_button4 = tk.Button(), FE3_ALDO_En2_onoff_button4 = tk.Button()
+FE4_ALDO_En2_onoff_button4 = tk.Button(), FE12_ALDO_En1_onoff_button4 = tk.Button()
+PCC_A_EN_IV8_2_onoff_button4 = tk.Button(), PCC_A_EN_IV8_1_onoff_button4 = tk.Button()
+FE11_ALDO_En1_onoff_button4 = tk.Button(), FE2_ALDO_En1_onoff_button4 = tk.Button()
+FE4_ALDO_En1_onoff_button4 = tk.Button(), FE11_ALDO_En2_onoff_button4 = tk.Button()
+FE2_ALDO_En2_onoff_button4 = tk.Button(), FE1_ALDO_En1_onoff_button4 = tk.Button()
+FE5_ALDO_En2_onoff_button4 = tk.Button(), FE1_ALDO_En2_onoff_button4 = tk.Button()
+FE8_ALDO_En1_onoff_button4 = tk.Button(), FE8_ALDO_En2_onoff_button4 = tk.Button()
+
+GBT_SCA4_button_array = []
+
+### GBT SCA4 setclr buttons ###
+FE9_ALDO_En2_setclr_button4 = tk.Button(), FE9_ALDO_En1_setclr_button4 = tk.Button()
+PCC_B_EN_IV8_1_setclr_button4 = tk.Button(), PCC_B_EN_IV8_2_setclr_button4 = tk.Button()
+cSS_B_setclr_button4 = tk.Button(), FE10_ALDO_En1_setclr_button4 = tk.Button()
+FE10_ALDO_En2_setclr_button4 = tk.Button(), cEN_B_setclr_button4 = tk.Button()
+FE7_ALDO_En1_setclr_button4 = tk.Button(), FE7_ALDO_En2_setclr_button4 = tk.Button()
+eSS_B_setclr_button4 = tk.Button(), FE3_ALDO_En1_setclr_button4 = tk.Button()
+FE5_ALDO_En1_setclr_button4 = tk.Button(), oEN_B_setclr_button4 = tk.Button()
+FE6_ALDO_En1_setclr_button4 = tk.Button(), FE6_ALDO_En2_setclr_button4 = tk.Button()
+FE12_ALDO_En2_setclr_button4 = tk.Button(), FE3_ALDO_En2_setclr_button4 = tk.Button()
+FE4_ALDO_En2_setclr_button4 = tk.Button(), FE12_ALDO_En1_setclr_button4 = tk.Button()
+PCC_A_EN_IV8_2_setclr_button4 = tk.Button(), PCC_A_EN_IV8_1_setclr_button4 = tk.Button()
+FE11_ALDO_En1_setclr_button4 = tk.Button(), FE2_ALDO_En1_setclr_button4 = tk.Button()
+FE4_ALDO_En1_setclr_button4 = tk.Button(), FE11_ALDO_En2_setclr_button4 = tk.Button()
+FE2_ALDO_En2_setclr_button4 = tk.Button(), FE1_ALDO_En1_setclr_button4 = tk.Button()
+FE5_ALDO_En2_setclr_button4 = tk.Button(), FE1_ALDO_En2_setclr_button4 = tk.Button()
+FE8_ALDO_En1_setclr_button4 = tk.Button(), FE8_ALDO_En2_setclr_button4 = tk.Button()
+
+GBT_SCA4_button_array2 = []
+
+### GBT SCA1 onoff buttons ###
+FE2_ALDO_En2_onoff_button1 = tk.Button(), FE5_ALDO_En2_onoff_button1 = tk.Button()
+FE8_ALDO_En2_onoff_button1 = tk.Button(), FE8_ALDO_En1_onoff_button1 = tk.Button()
+FE11_ALDO_En2_onoff_button1 = tk.Button(), FE1_ALDO_En2_onoff_button1 = tk.Button()
+FE1_ALDO_En1_onoff_button1 = tk.Button(), FE11_ALDO_En1_onoff_button1 = tk.Button()
+FE2_ALDO_En1_onoff_button1 = tk.Button(), FE4_ALDO_En1_onoff_button1 = tk.Button()
+FE12_ALDO_En1_onoff_button1 = tk.Button(), PCC_A_EN_IV8_2_onoff_button1 = tk.Button()
+PCC_A_EN_IV8_1_onoff_button1 = tk.Button(), FE12_ALDO_En2_onoff_button1 = tk.Button()
+FE3_ALDO_En2_onoff_button1 = tk.Button(), FE4_ALDO_En2_onoff_button1 = tk.Button()
+oEN_A_onoff_button1 = tk.Button(), FE6_ALDO_En1_onoff_button1 = tk.Button()
+FE6_ALDO_En2_onoff_button1 = tk.Button(), eSS_A_onoff_button1 = tk.Button()
+FE3_ALDO_En1_onoff_button1 = tk.Button(), FE5_ALDO_En1_onoff_button1 = tk.Button()
+cEN_A_onoff_button1 = tk.Button(), FE7_ALDO_En1_onoff_button1 = tk.Button()
+FE7_ALDO_En2_onoff_button1 = tk.Button(), cSS_A_onoff_button1 = tk.Button()
+FE9_ALDO_En2_onoff_button1 = tk.Button(), FE9_ALDO_En1_onoff_button1 = tk.Button()
+PCC_B_EN_IV8_2_onoff_button1 = tk.Button(), FE10_ALDO_En1_onoff_button1 = tk.Button()
+FE10_ALDO_En2_onoff_button1 = tk.Button(), PCC_B_EN_IV8_1_onoff_button1 = tk.Button()
+
+GBT_SCA1_button_array = []
+
+### GBT SCA1 setclr buttons ###
+FE2_ALDO_En2_setclr_button1 = tk.Button(), FE5_ALDO_En2_setclr_button1 = tk.Button()
+FE8_ALDO_En2_setclr_button1 = tk.Button(), FE8_ALDO_En1_setclr_button1 = tk.Button()
+FE11_ALDO_En2_setclr_button1 = tk.Button(), FE1_ALDO_En2_setclr_button1 = tk.Button()
+FE1_ALDO_En1_setclr_button1 = tk.Button(), FE11_ALDO_En1_setclr_button1 = tk.Button()
+FE2_ALDO_En1_setclr_button1 = tk.Button(), FE4_ALDO_En1_setclr_button1 = tk.Button()
+FE12_ALDO_En1_setclr_button1 = tk.Button(), PCC_A_EN_IV8_2_setclr_button1 = tk.Button()
+PCC_A_EN_IV8_1_setclr_button1 = tk.Button(), FE12_ALDO_En2_setclr_button1 = tk.Button()
+FE3_ALDO_En2_setclr_button1 = tk.Button(), FE4_ALDO_En2_setclr_button1 = tk.Button()
+oEN_A_setclr_button1 = tk.Button(), FE6_ALDO_En1_setclr_button1 = tk.Button()
+FE6_ALDO_En2_setclr_button1 = tk.Button(), eSS_A_setclr_button1 = tk.Button()
+FE3_ALDO_En1_setclr_button1 = tk.Button(), FE5_ALDO_En1_setclr_button1 = tk.Button()
+cEN_A_setclr_button1 = tk.Button(), FE7_ALDO_En1_setclr_button1 = tk.Button()
+FE7_ALDO_En2_setclr_button1 = tk.Button(), cSS_A_setclr_button1 = tk.Button()
+FE9_ALDO_En2_setclr_button1 = tk.Button(), FE9_ALDO_En1_setclr_button1 = tk.Button()
+PCC_B_EN_IV8_2_setclr_button1 = tk.Button(), FE10_ALDO_En1_setclr_button1 = tk.Button()
+FE10_ALDO_En2_setclr_button1 = tk.Button(), PCC_B_EN_IV8_1_setclr_button1 = tk.Button()
+
+GBT_SCA1_button_array2 = []
+
+### LpGBT3 onoff buttons and labels ###
+N13_onoff_button3 = tk.Button(), N12_onoff_button3 = tk.Button()
+clkEnS_onoff_button3 = tk.Button(), PCC_A_PG_1V8_2_onoff_label3 = tk.Label()
+PCC_A_PG_1V2_onoff_label3 = tk.Label(), PCC_A_PG_1V8_1_onoff_label3 = tk.Label()
+calEnPE_onoff_button3 = tk.Button(), calEnS_onoff_button3 = tk.Button()
+eEnPE_onoff_button3 = tk.Button(), eEnS_onoff_button3 = tk.Button()
+clkEnPE_onoff_button3 = tk.Button(), N8_onoff_button3 = tk.Button()
+L7_onoff_button3 = tk.Button(), L6_onoff_button3 = tk.Button()
+L5_onoff_button3 = tk.Button(), R3_onoff_button3 = tk.Button()
+
+### LpGBT3 setclr buttons and labels ###
+N13_setclr_button3 = tk.Button(), N12_setclr_button3 = tk.Button()
+clkEnS_setclr_button3 = tk.Button(), PCC_A_PG_1V8_2_setclr_label3 = tk.Label()
+PCC_A_PG_1V2_setclr_label3 = tk.Label(), PCC_A_PG_1V8_1_setclr_label3 = tk.Label()
+calEnPE_setclr_button3 = tk.Button(), calEnS_setclr_button3 = tk.Button()
+eEnPE_setclr_button3 = tk.Button(), eEnS_setclr_button3 = tk.Button()
+clkEnPE_setclr_button3 = tk.Button(), N8_setclr_button3 = tk.Button()
+L7_setclr_button3 = tk.Button(), L6_setclr_button3 = tk.Button()
+L5_setclr_button3 = tk.Button(), R3_setclr_button3 = tk.Button()
+
+### LpGBT2 onoff buttons and labels ###
+clkEnPE_L1_onoff_button2 = tk.Button(), eEnS_L1_onoff_button2 = tk.Button()
+UnusedL1_7_onoff_button2 = tk.Button(), calEnS_L1_onoff_button2 = tk.Button()
+UnusedL1_3_onoff_button2 = tk.Button(), clkEnS_L1_onoff_button2 = tk.Button()
+PCC_B_PG_1V8_1_onoff_label2 = tk.Label(), UnusedL1_6_onoff_button2 = tk.Button()
+eEnPE_L1_onoff_button2 = tk.Button(), UnusedL1_1_onoff_button2 = tk.Button()
+UnusedL1_5_onoff_button2 = tk.Button(), calEnPE_L1_onoff_button2 = tk.Button()
+PCC_B_PG_1V8_2_onoff_label2 = tk.Label(), Unused_L1_2_onoff_button2 = tk.Button()
+PCC_B_PG_2V5_onoff_label2 = tk.Label(), UnusedL1_4_onoff_button2 = tk.Button()
+
+### LpGBT2 setclr buttons and labels ###
+clkEnPE_L1_setclr_button2 = tk.Button(), eEnS_L1_setclr_button2 = tk.Button()
+UnusedL1_7_setclr_button2 = tk.Button(), calEnS_L1_setclr_button2 = tk.Button()
+UnusedL1_3_setclr_button2 = tk.Button(), clkEnS_L1_setclr_button2 = tk.Button()
+PCC_B_PG_1V8_1_setclr_label2 = tk.Label(), UnusedL1_6_setclr_button2 = tk.Button()
+eEnPE_L1_setclr_button2 = tk.Button(), UnusedL1_1_setclr_button2 = tk.Button()
+UnusedL1_5_setclr_button2 = tk.Button(), calEnPE_L1_setclr_button2 = tk.Button()
+PCC_B_PG_1V8_2_setclr_label2 = tk.Label(), Unused_L1_2_setclr_button2 = tk.Button()
+PCC_B_PG_2V5_setclr_label2 = tk.Label(), UnusedL1_4_setclr_button2 = tk.Button()
+
+### Scripts tab buttons and labels ###
+### GBT-SCA4 ###
+Connect_button = tk.Button(), EnableGPIO_button = tk.Button()
+EnableAtoD_button = tk.Button(), Bread_button = tk.Button()
+DIRread_button = tk.Button(), DATAOUTread_button = tk.Button()
+IDread_button = tk.Button(), GPIOon_button = tk.Button()
+GPIOset_button = tk.Button(), GPIOclr_button = tk.Button()
+GPIOoff_button = tk.Button(), Bread_label = tk.Label()
+DIRread_label = tk.Label(), DATAOUTread_label = tk.Label()
+IDread_label = tk.Label(), GPIO_entry = tk.Entry()
+
+### GBT-SCA1 ###
+Connect_button2 = tk.Button(), EnableGPIO_button2 = tk.Button()
+EnableAtoD_button2 = tk.Button(), Bread_button2 = tk.Button()
+DIRread_button2 = tk.Button(), DATAOUTread_button2 = tk.Button()
+IDread_button2 = tk.Button(), GPIOon_button2 = tk.Button()
+GPIOset_button2 = tk.Button(), GPIOclr_button2 = tk.Button()
+GPIOoff_button2 = tk.Button(), Bread_label2 = tk.Label()
+DIRread_label2 = tk.Label(), DATAOUTread_label2 = tk.Label()
+IDread_label2 = tk.Label()
+
+
+output_textbox = tk.Text()
+
