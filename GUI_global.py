259e7c14ee0a67359b862f9f620006f944015301
Sedrick William Weinschenk <sedrick.william.weinschenk@cern.ch>
2022-04-01 22:09:01 +0200

    Added LpGBT arrays

--- GUI_global.py
+++ GUI_global.py
@@ -1,11 +1,12 @@
 refreshTime = 10
 stopAutomaticRefresh = False
 
-trID = 1
-
 GBT_SCA4_button_array = []
 GBT_SCA4_button_array2 = []
 GBT_SCA1_button_array = []
 GBT_SCA1_button_array2 = []
 
-
+LpGBT3_buttonlabel_array = []
+LpGBT3_buttonlabel_array2 = []
+LpGBT2_buttonlabel_array = []
+LpGBT3_buttonlable_array2 = []
